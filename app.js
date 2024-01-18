6c14b6e69fd0dcd26ed448b6674f21040e8ff95f
Kuruva Pavani <pavanikuruva2109@gmail.com>
2024-01-18 09:53:13 +0530

    final commit

--- app.js
+++ app.js
@@ -4,28 +4,17 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require("lodash");
 const mongoose = require("mongoose");
-
-// Use the new URL parser and remove deprecated options
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/postsDB", {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-}, { createIndexes: true });
-
-const Post = mongoose.model("Post", postSchema);
-
-
-// Rest of your code...
-
+require("dotenv").config();
+mongoose.connect(process.env.DATA_URL+"/postsDB");
+const postSchema = {
+  title : String,
+  content : String
+}
+const Post = mongoose.model("Post",postSchema);
 // let posts = [];
-const homeStartingContent = "home Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "about Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "contact Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "# Welcome to My Weekly Achievements Blog .Hi, I'm Kuruva Pavani, a web developer and a lifelong learner. I created this blog to document and share my weekly achievements in coding, learning, and personal growth.Every week, I post about the new skills I learned, the projects I completed, and the challenges I faced. I also share the resources, tools, and tips that helped me along the way.My goal is to inspire and motivate other developers and learners to pursue their passions, overcome their obstacles, and achieve their goals.If you are interested in web development, MongoDB, Node, Express, or any other topics related to coding and learning, you are in the right place!.Here are some of my latest posts. You can view a post by clicking on See More which redirects to another page with the post id..You can find me on:- [LinkedIn](https://www.linkedin.com/in/kuruva-pavani-10388b27b)- [GitHub](https://github.com/kuruvapavani).I would love to hear from you, so feel free to reach out and say hi!. Thank you for visiting my blog. I hope you enjoyed it and found it useful. See you next week!";
+const aboutContent = "Hello, and welcome to my blog! My name is Kuruva Pavani, and I am a web developer and a lifelong learner. I created this blog to document and share my weekly achievements in coding, learning, and personal growth.I have always been passionate about web development and technology.I enjoy building websites and applications that are functional, beautiful, and user-friendly.I also love learning new things and challenging myself. I believe that learning is a lifelong journey, and that we can always improve ourselves and our skills. That's why I set weekly goals for myself and track my progress on this blog. I write about the new skills I learned, the projects I completed, and the challenges I faced. I also share the resources, tools, and tips that helped me along the way.You can check my other projects on github. Feel free to contact me";
+const contactContent = "If you have any questions, comments, or suggestions about my blog or my weekly achievements, I would love to hear from you. You can also contact me if you want to collaborate, hire me, or just say hi. You can reach me by sending me an email at [pavanikuruva2109@gmail.com]. You can also follow me on [instagram](pavani._21) , [LinkedIn](https://www.linkedin.com/in/kuruva-pavani-10388b27b). I will do my best to get back to you within 24 hours. I usually reply by email, but if you prefer a different method, please let me know in your message. Thank you for reaching out to me. I appreciate your interest and support. I look forward to hearing from you and reading your message.";
 
 const home = new Post({
   title : "Home",
@@ -61,11 +50,6 @@ app.get("/compose",function(req,res){
   res.render(__dirname + "/views/compose.ejs",{homepage : contactContent });
 })
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port = 3000;
-}
-
 app.post("/",function(req,res){
   const postTitle = req.body.title;
   const postContent = req.body.content;
@@ -78,7 +62,6 @@ app.post("/",function(req,res){
 });
 
 
-
 app.get("/posts/:testing",function(req,res){
   Post.find({_id : req.params.testing}).then(posts=>{
     posts.forEach(function(post){
@@ -89,8 +72,12 @@ app.get("/posts/:testing",function(req,res){
       console.error (err);
     });
 })
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
 
 
 app.listen(port, function() {
-  console.log("Server started on port " + port);
+  console.log("Server started on port 3000");
 });
